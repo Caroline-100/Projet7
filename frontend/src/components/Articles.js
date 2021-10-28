2afc4f1094b86ff6a17b16f9730b99c1c2d61d99
Caroline-100 <47947548+Caroline-100@users.noreply.github.com>
2021-10-28 23:46:58 +0200

    use redirect history,redirect articles Display One product

--- frontend/src/components/Articles.js
+++ frontend/src/components/Articles.js
@@ -1,44 +1,63 @@
 import React from "react";
 
-const infos = {
-  user: {
-    date: "Post at 3 hours ago",
-    id: `user boo`,
-  },
-};
-function UserInfos(props) {
-  return (
-    <div className="UserInfos">
-      <h3>{props.user.id}</h3>
-      <h3 className="post-hours-users">{props.user.date}</h3>
-    </div>
-  );
-}
+import { useHistory } from "react-router-dom";
 
-function NormalButtonShareAndComment() {
-  return (
-    <div>
-      <button className="Share">
-        Share <i class="fas fa-bullhorn"></i>
-      </button>
-      <button className="comment">
-        Comment <i className="fas fa-comment"></i>
-      </button>
-    </div>
-  );
-}
+// const infos = {
+//   user: {
+//     date: "Post at 3 hours ago",
+//     id: `user boo`,
+//   },
+// };
+// function UserInfos(props) {
+//   return (
+//     <div className="UserInfos">
+//       <h3>{props.user.id}</h3>
+//       <h3 className="post-hours-users">{props.user.date}</h3>
+//     </div>
+//   );
+// }
+
+// function NormalButtonShareAndComment() {
+//   return (
+//     <div>
+//       <button className="Share">
+//         Share <i className="fas fa-bullhorn"></i>
+//       </button>
+//       <button className="comment">
+//         Comment <i className="fas fa-comment"></i>
+//       </button>
+//     </div>
+//   );
+// }
+
+// that you may use to navigate.import { useHistory } from "react-router-dom";
+
+
+
 
 function Articles(props) {
-  console.log(props);
+  let history = useHistory();
+  
+  function handleClick(event) {
+    console.log(typeof event.target.value);
+    localStorage.setItem('value', event.target.value );
+    history.push(`/DisplayOneProduct`);
+  }
+  // function handleChange(event) {
+  //   console.log(document.location.href);
+  //   let newURL = new URLSearchParams('/');
+  //   newURL.set('id', event.target.value);
+  
   return (
-    <article className="post-article">
-      <div className="user">
-        <UserInfos user={infos.user} />
-        <h3>description : {props.name}</h3>
-        <img src={props.imgUrl} alt={props.alt} />
-      </div>
-      <NormalButtonShareAndComment />
-    </article>
+    <li key={props.id} value={props.id}>
+      
+      <h1>createdAt : {props.createdAt}</h1>
+      <h2>text: {props.text}</h2>
+      <h3>PersonId : {props.PersonId}</h3>
+      <h4>updateAt : {props.updatedAt}</h4>
+      <button onClick={handleClick} className={props.id} value={props.id}
+      >but</button>
+    </li>
   );
 }
 export default Articles;
