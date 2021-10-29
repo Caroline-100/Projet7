714694ce7c03dcf52b3bf18328115a567836dfde
Caroline-100 <47947548+Caroline-100@users.noreply.github.com>
2021-10-29 21:53:58 +0200

    add routes find a post + find all user + find all posts

--- backend/app.js
+++ backend/app.js
@@ -2,15 +2,13 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-// const file_System = require('file-system');
 // const multer = require('./middleware/multer');
 const authenfication = require('./middleware/auth');
 const {
   Person,
   Post,
 } = require('./models');
-
+// const upload = require('express-fileupload');
 const app = express();
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -24,11 +22,12 @@ app.use((req, res, next) => {
   );
   next();
 });
-app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.use("/api/auth", userRouter);
+
+// api find one user
 app.get('/', (request, response) => {
   Person.findOne()
     .then((users) => {
@@ -38,7 +37,39 @@ app.get('/', (request, response) => {
       console.error(err);
     });
 });
-
+// api find all POST
+app.get('/post', authenfication, (req, res) => {
+  Post.findAll()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+// api find all USER
+app.get('/user', authenfication, (req, res) => {
+  Person.findAll()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+// find one post
+app.get('/post/:id', authenfication, (req, res) => {
+  Post.findOne(
+    {
+      where: { id: req.params.id },
+    },
+  ).then((post) => {
+    res.json(post);
+  }).catch((err) => {
+    console.error(err);
+  });
+});
+// signup
 app.post('/signup', (req, res) => {
   if (!req.body) {
     res.status(400).send({
@@ -67,31 +98,7 @@ app.post('/signup', (req, res) => {
       });
   }
 });
-// app.put("/users/:id", async (req, res, next) => {
-//   const updatedUser = await db.Person.update(
-//     { email: req.body.email },
-//     { where: { id: req.params.id } }
-//   ).catch((error) => {
-//     console.error(error);
-//     res.send(500);
-//   });
-//   console.log({ updatedUser });
-//   res.send(updatedUser);
-// });
-
-// // .update({ email: req.body.email }, { where: { id: req.body.id } })
-
-// app.get('/setcookie', (req, res) => {
-//   res.cookie('Cookie token name`,a, {
-//     maxAge: 5000,
-//     // expires works the same as the maxAge
-//     expires: new Date('01 12 2021'),
-//     secure: true,
-//     httpOnly: true,
-//     sameSite: 'lax',
-//   });
-//   res.send('Cookie have been saved successfully');
-// });
+//login
 app.post('/login', (req, res) => Person.findOne({
   where: {
     username: req.body.username,
@@ -133,16 +140,10 @@ app.post('/login', (req, res) => Person.findOne({
   }));
 // post articles with authenfication and add id of in database posts
 app.post('/posts', authenfication, (req, res) => {
-  // if (req.file) {
-  //   console.log('file');
-  // } else {
-  //   console.error('no req.file');
-  // }
   Post.create({
     text: req.body.text,
     PersonId: req.context.userId,
   })
-  // data:`${req.protocol}://${req.get('host')}/image/${req.file.filename}`,
   // if the post is created return a json postCreated
     .then((postCreated) => {
       console.log({ postCreated });
@@ -156,5 +157,43 @@ app.post('/posts', authenfication, (req, res) => {
       res.sendStatus(400);
     });
 });
+// // multer
+// app.post('/images', multer, (req, res) => {
+//   if (req.body) {
+//     console.log(`image${req.body}`);
+//   } else {
+//     console.error('noimage');
+//   }
+//   // ??????
+//   // const imageObject = JSON.parse(req.body.data);
+//   // console.log(imageObject);
+//   // Post.create({
+//   //   text: req.body.text,
+//   //   PersonId: req.context.userId,
+//   //   data: `${req.protocol}://${req.get('host')}/image/${req.file.filename}`,
+//   // }).then((imagecreated) => {
+//   //   console.log(imagecreated);
+//   //   console.log({ imagecreated: imagecreated.toJSON() });
+//   //   res.send(imagecreated);
+//   // }).catch((error) => {
+//   //   console.error(error);
+//   //   res.sendStatus(400);
+//   // });
+// });
+
 
 module.exports = app;
+
+// app.put("/users/:id", async (req, res, next) => {
+//   const updatedUser = await db.Person.update(
+//     { email: req.body.email },
+//     { where: { id: req.params.id } }
+//   ).catch((error) => {
+//     console.error(error);
+//     res.send(500);
+//   });
+//   console.log({ updatedUser });
+//   res.send(updatedUser);
+// });
+
+// // .update({ email: req.body.email }, { where: { id: req.body.id } })
