533a1317170f0d223c498e6297848e33ecc21009
Caroline-100 <47947548+Caroline-100@users.noreply.github.com>
2021-10-30 14:13:21 +0200

    new file post and user controllers

---
+++ backend/controllers/post.js
@@ -0,0 +1,45 @@
+const {
+  Post,
+} = require('../models');
+
+// find one post
+exports.findPost = (req, res) => {
+  Post.findOne(
+    {
+      where: { id: req.params.id },
+    },
+  ).then((post) => {
+    res.json(post);
+  }).catch((err) => {
+    console.error(err);
+  });
+};
+
+// api find all POST
+exports.findAlls = (req, res) => {
+  Post.findAll()
+    .then((users) => { res.json(users); })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+// create a post
+exports.createPost = (req, res) => {
+  Post.create({
+    title: req.body.title,
+    text: req.body.text,
+    PersonId: req.context.userId,
+  })
+  // if the post is created return a json postCreated
+    .then((postCreated) => {
+      console.log({ postCreated });
+      console.log({ postCreated: postCreated.toJSON() });
+      res.send(postCreated);
+    })
+  // if the post isn't created return error
+    .catch((error) => {
+      console.error(error);
+      res.sendStatus(400);
+    });
+};
---
+++ backend/controllers/user.js
@@ -0,0 +1,96 @@
+// const express = require('express');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+const {
+  Person,
+} = require('../models');
+
+exports.signup = (req, res) => {
+  if (!req.body) {
+    res.status(400).send({
+      message: ' Content can not be empty',
+    });
+  } else {
+    bcrypt
+      .hash(req.body.password, 10)
+      .then((hash) => {
+        Person.create({
+          email: req.body.email,
+          username: req.body.username,
+          password: hash,
+        })
+          .then(() => {
+            res.sendStatus(201);
+          })
+          .catch((error) => {
+            console.error(error);
+            res.status(400).send(error.message);
+          });
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(400).send(error.message);
+      });
+  }
+};
+
+exports.login = (req, res) => Person.findOne({
+  where: {
+    username: req.body.username,
+    // password: req.body.password,
+  },
+})
+  .then((user) => {
+    console.log(user);
+    if (!user) {
+      res.status(401).json({ message: 'User Not found' });
+    }
+    // check if pass are founded in db and request.
+    console.log('mdp :', req.body.password, user, user.dataValues.id);
+    // use bcrypt for compare password exists
+    return bcrypt
+      .compare(req.body.password, user.dataValues.password)
+      // promise if is bcript is good
+      .then((valid) => {
+        // assign a token of validation at user_id,
+        const accessToken = jwt.sign(
+          { userId: user.dataValues.id },
+          'Random_Secret_token',
+          { expiresIn: '24h' },
+        );
+        if (!valid) {
+          return res.status(401).json({ message: 'password invalid' });
+        }
+        // if compare is ok and token is ok send token
+        return res.status(200).json(accessToken);
+      })
+      // pb connexion (error server)
+      .catch((error) => {
+        res.status(500).send(error);
+      });
+  })
+  // catch error if user has a problem.
+  .catch((error) => {
+    res.send(error);
+  });
+
+exports.oneUser = (request, response) => {
+  Person.findOne()
+    .then((users) => {
+      response.json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+exports.allUser = (req, res) => {
+  Person.findAll()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
