e64316dc7b60c9298e836b92c1ff05fc774a8138
Caroline-100 <47947548+Caroline-100@users.noreply.github.com>
2021-11-06 12:11:48 +0100

    change name route POST -> POSTS, delete function clickONchange, add image display posts, delete componentWillUnmount()=> not used, comment button logout

--- frontend/src/components/DisplayPosts.js
+++ frontend/src/components/DisplayPosts.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Articles from './Articles'
 
+
 class DisplayPosts extends React.Component {
     constructor() {
         super()
@@ -14,18 +15,13 @@ class DisplayPosts extends React.Component {
                 updateAt:null,
             }
         }
-        this.clickOnchange = this.clickOnchange.bind = this;
     }
-    clickOnchange(event) {
-        console.log('click');
-    }
-
     async componentDidMount() {
 
         const option = {};
         const token = localStorage.getItem('datee');
         option.headers = {authorization : `Bearer ${token}`,   }
-        const url = 'http://localhost:3004/post';
+        const url = 'http://localhost:3004/posts';
         const response = await fetch(url, option);
 
         const data = await response.json();
@@ -33,9 +29,9 @@ class DisplayPosts extends React.Component {
         const arra =  data.map((user) => {
 
             return <Articles key={user.id.toString()} 
-                    onclick={this.clickOnchange} 
                     id={user.id}text={user.text}
                     updatedAt={user.updatedAt}
+                    data={user.data}
                     PersonId={user.PersonId} 
                     createdAt={user.createdAt} />
         })
@@ -43,6 +39,12 @@ class DisplayPosts extends React.Component {
             array: arra,
         })
     }
+    // componentWillUnmount() {
+    //     localStorage.clear()
+    //  //    if(!localStorage.getItem('datee')) {
+    //  //      this.props.history.push('/Login')
+    //  //    }
+    //  }
 
         render() {
         return (
@@ -50,9 +52,13 @@ class DisplayPosts extends React.Component {
                 <ul>
                 {this.state.array}
                 </ul>
+                {/* <button onClick={this.logout}>
+          LogOut
+        </button> */}
             </div> 
         )
     }
 }
+
 
 export default DisplayPosts;
