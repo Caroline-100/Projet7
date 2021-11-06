bd435843fa5024ac62b768628b97d3bf2f1bd198
Caroline-100 <47947548+Caroline-100@users.noreply.github.com>
2021-11-06 11:45:59 +0100

    delete title ,add logo entreprise

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,9 +1,14 @@
 import React from "react";
 
+import logo from '../image/icon-left-font-monochrome-black.svg'; // Tell webpack this JS file uses this image
+
+console.log(logo); // /logo.84287d09.png
+
 function Nav() {
   return (
     <nav className="navbar">
-      <h3>Groupomania</h3>
+      <img src={logo}
+           alt='logo-entreprise'></img>
     </nav>
   );
 }
