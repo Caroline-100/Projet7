bee17a60b73c8860223a7ff299ebe3ef7f6c40b9
Caroline-100 <47947548+Caroline-100@users.noreply.github.com>
2021-11-06 12:12:57 +0100

    change state when is log and logout, redirect page (not optimal

--- frontend/src/components/LogINorLogOUT.js
+++ frontend/src/components/LogINorLogOUT.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import {Redirect} from 'react-router-dom';
 
 class LogINorLogOUT extends React.Component {
     constructor () {
         super()
         this.state = {
-            isLoggedIn:true,
+            isLoggedIn:false,
         };
         this.handleclick = this.handleclick.bind(this)
     }    
@@ -13,11 +14,19 @@ class LogINorLogOUT extends React.Component {
             console.log(previusState);
             return {isLoggedIn :!previusState.isLoggedIn}
         })
+
+    }
+    renderRedirect() {
+        if(this.state.isLoggedIn)
+        {
+            return <Redirect to='/LogIn'/>
+        }
     }
     render() {
             let buttonText = this.state.isLoggedIn ? "login" : "logout"
         return (
         <div> 
+            {this.renderRedirect()}
         <button onClick={this.handleclick}>{buttonText}
             
         </button>
