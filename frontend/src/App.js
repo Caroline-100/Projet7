6ebadf77f0b0b441b06bea605dbbe7776f920888
Caroline-100 <47947548+Caroline-100@users.noreply.github.com>
2021-11-06 11:51:14 +0100

    delete imports, add Switch, import boostrap, add functionalities login : isAuthentificated, delete routes not used

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,46 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 // import files component
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
 import Nav from "./components/navbar";
 import LogIn from "./components/LogIn";
 import SignUp from "./components/SignUp";
 import Main from "./components/Main";
-import LogINorLogOUT from "./components/LogINorLogOUT";
 import CreatePost from "./components/createPost";
 import DisplayPosts from "./components/DisplayPosts";
 import DisplayOneProduct from './components/DisplayOneProduct'
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      isAuthentificated: false,
+      user: null,
+      token: null,
+    }
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+  handleLogin(isAuthentificated) {
+    this.setState({ isAuthentificated})
+  }
   render() {
+    //extra value 
+    const { isAuthentificated } = this.state;
     return (
       <BrowserRouter>
         <Nav />
-
-        <Route path="/LogIn" exact component={LogIn} />
-        <Route path="/SignUp" exact component={SignUp} />
-        <Route path="/Main" exact component={Main} />
-        <Route path="/LogINorLogOUT" exact component={LogINorLogOUT} />
-        <Route path="/CreatePost" exact component={CreatePost} />    
-        <Route path="/DisplayPosts" exact component={DisplayPosts} />  
-        <Route path="/DisplayOneProduct" exact component={DisplayOneProduct}/>          
+        <Switch>
+          <Route path="/SignUp" exact component={SignUp} />
+          <Route path="/LogIn" render={() => !isAuthentificated ? <LogIn isLogin={this.handleLogin} /> : <DisplayPosts />} />
+          <Route path="/Main" exact component={Main} />
+          <Route path="/CreatePost" exact component={CreatePost} />
+          <Route path="/DisplayPosts" exact component={DisplayPosts} />
+          <Route path='/DisplayOneProduct/:id' exact component={DisplayOneProduct} />
+        </Switch>
       </BrowserRouter>
     );
   }
