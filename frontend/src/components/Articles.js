f6a3286d286fadb0385f858c18076471e17f239b
Caroline-100 <47947548+Caroline-100@users.noreply.github.com>
2021-11-06 11:54:10 +0100

    add Bootstrap , modification style css,  cgange style article use functionality bootstrap

--- frontend/src/components/Articles.js
+++ frontend/src/components/Articles.js
@@ -1,28 +1,36 @@
 import React from "react";
 
 import { useHistory } from "react-router-dom";
-
+import { Card, Button } from 'react-bootstrap';
+// import Button from "@restart/ui/esm/Button";
 function Articles(props) {
   let history = useHistory();
-  
+
+
   function handleClick(event) {
     console.log(typeof event.target.value);
-    localStorage.setItem('value', event.target.value );
-    history.push(`/DisplayOneProduct`);
+    localStorage.setItem('value', event.target.value);
+    history.push(`/DisplayOneProduct/${localStorage.getItem('value')}`);
   }
   return (
-    <li key={props.id} value={props.id}>
-      <div className='article-top'>
-      <h1 className='inherited'>createdAt : {props.createdAt}</h1>
-      <h2>text: {props.text}</h2>
-      <h3>PersonId : {props.PersonId}</h3>
-      <h4>updateAt : {props.updatedAt}</h4>
-      <div className='buttons'>
-      <button onClick={handleClick} className={props.id} value={props.id}>
-      Share</button>
-      </div>
-      </div>
-    </li>
+
+    <Card style={{ width: '37rem' }}>
+       {/* <Card.Img variant="top" src="image" alt='imgpost' /> */}
+       <Card.Body>
+       <Card.Title>Card Title : {props.title}</Card.Title>
+      {/* <li key={props.id} value={props.id}> */}
+          <Card.Text>text: {props.text}
+                     <Card.Subtitle>createdAt : {props.createdAt}</Card.Subtitle>
+                     <Card.Subtitle>PersonIds : {props.PersonId}</Card.Subtitle>
+                     Data: {props.data}
+          </Card.Text>
+          {/* <img src='https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350' alt={props.image.name} /> */}
+          <Card.Text>updateAt : {props.updatedAt}</Card.Text>
+          <Button onClick={handleClick} className={props.id} value={props.id}>
+          Share</Button>
+      {/* </li> */}
+       </Card.Body>
+    </Card>
   );
 }
 export default Articles;
