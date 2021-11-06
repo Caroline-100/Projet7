68de95566f95a7220a1ce35b517ec6bd7663b6d2
Caroline-100 <47947548+Caroline-100@users.noreply.github.com>
2021-11-06 11:48:44 +0100

    add bootstrap, change input to button submit, change type password, delete componentDidMount

--- frontend/src/components/LogIn.js
+++ frontend/src/components/LogIn.js
@@ -1,7 +1,7 @@
 
 import React from "react";
-
-
+import {Button} from 'react-bootstrap';
+// import Display from './changepage'
 class LogIn extends React.Component {
   constructor() {
     super();
@@ -16,26 +16,52 @@ class LogIn extends React.Component {
     const { name, value } = event.target;
     this.setState({ [name]: value });
   }
+
   submit(event) {
     event.preventDefault();
     const data = {
       username: this.state.username,
       password: this.state.password,
     };
-
-    console.log("this is", data);
     const newLocal = 'http://localhost:3004/login';
     fetch(newLocal, {
       method: "POST",
-      headers:{ 
-        "Content-type": "application/json" },
+      headers: {
+        "Content-type": "application/json"
+      },
       body: JSON.stringify(data),
+      
     }).then(response => response.json())
-      .then((token)=> 
-        { const datatoken = token;
-          localStorage.setItem('datee', datatoken);
-        }) 
+    .then((token) => {
+      const datatoken = token;
+      localStorage.setItem('datee', datatoken);
+      if (localStorage.getItem('datee') !== null) {
+
+        this.props.isLogin(true)
+        this.props.history.push('/DisplayPosts');
+      }
+    }).catch((error) => {
+      console.error(error)
+    })
   }
+
+  //   componentDidMount() {
+
+  //     if(!localStorage.getItem('datee')) {
+  //       // window.location.reload(false)
+  //       this.props.history.push('/Login')
+
+  //     }
+  //       this.props.history.push('/DisplayPosts')
+
+  //   }
+
+  //   logout() {
+  //     localStorage.clear()
+  //  //    if(!localStorage.getItem('datee')) {
+  //  //      this.props.history.push('/Login')
+  //  //    }
+  //  }
 
   render() {
     return (
@@ -58,7 +84,7 @@ class LogIn extends React.Component {
           <div className="form-div">
             <label htmlFor="name">Password</label>
             <input
-              type="text"
+              type="password"
               className="name"
               name="password"
               value={this.state.password}
@@ -69,17 +95,19 @@ class LogIn extends React.Component {
             />
           </div>
           <div className="div-btn-valid-register">
-            <input
+            <Button variant='success'
               name="login"
               className="input-Valid"
               type="submit"
               value="Valid"
-            />
+            > Valid</Button>
           </div>
         </form>
+        {/* <button onClick={this.logout}>logout</button> */}
         <p>
-              {this.state.password} ,{this.state.username}
+          {this.state.password} ,{this.state.username}
         </p>
+
       </main>
     );
   }
