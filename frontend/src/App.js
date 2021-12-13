fe2ffdebe90f901492dd8adef54ac27a5327a90f
Caroline-100 <47947548+Caroline-100@users.noreply.github.com>
2021-12-13 14:41:38 +0100

    delete auth stat, clean function not used

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
+// import react library
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-// import files component
-import "./App.css";
+
+
+//style
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-
-import Nav from "./components/navbar";
+import "./App.css";
+// import files components
+// import Nav from "./components/navbar";
 import LogIn from "./components/LogIn";
 import SignUp from "./components/SignUp";
 import Main from "./components/Main";
@@ -14,28 +15,22 @@ import CreatePost from "./components/createPost";
 import DisplayPosts from "./components/DisplayPosts";
 import DisplayOneProduct from './components/DisplayOneProduct'
 
+// routes components 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      isAuthentificated: false,
       user: null,
       token: null,
     }
-    this.handleLogin = this.handleLogin.bind(this);
-  }
-  handleLogin(isAuthentificated) {
-    this.setState({ isAuthentificated})
   }
   render() {
-    //extra value 
-    const { isAuthentificated } = this.state;
     return (
       <BrowserRouter>
-        <Nav />
+        {/* <Nav /> */}
         <Switch>
           <Route path="/SignUp" exact component={SignUp} />
-          <Route path="/LogIn" render={() => !isAuthentificated ? <LogIn isLogin={this.handleLogin} /> : <DisplayPosts />} />
+          <Route path="/LogIn" exact component={LogIn} />
           <Route path="/Main" exact component={Main} />
           <Route path="/CreatePost" exact component={CreatePost} />
           <Route path="/DisplayPosts" exact component={DisplayPosts} />
